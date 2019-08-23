899eae5d41f60fb7c526deacc525ac8167ac80d0
navasg <genenavas97@gmail.com>
2019-08-22 22:44:02 -0400

    More views

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,17 @@ $('.banner-slick').slick({
   cssEase: 'linear',
   autoplay: true,
 });
+
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+$('.carousel-anexo').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1
+});
+
